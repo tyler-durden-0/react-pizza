d38e8fa24750ae1476c1e2d9631a9a1cf9f3096f
Ivan <ivanchernetskey@mail.ru>
2021-03-26 19:38:06 +0300

    make button component function

--- src/Button.jsx
+++ src/Button.jsx
@@ -2,20 +2,14 @@ import React from 'react'
 
 import classNames from 'classnames'
 
-class Button extends React.Component {
-    componentDidMount() {
-        console.log('Компонент дыл замаунчен')
-    }
-
-
-    render() {
-        return <button className={classNames(
-            'button',
-            {
-            'button--outline': this.props.outline
-            }
-        )}>{this.props.children}</button>
-    }
+function Button(props) {
+    console.log(props)
+    return (
+        <button
+            className={classNames('button', {'button--outline': props.outline})}
+        >
+            {props.children}
+        </button>)
 }
 
 export default Button
