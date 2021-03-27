00a53165314e70552d405bd44b55f9bb249a0472
Ivan <ivanchernetskey@mail.ru>
2021-03-27 20:51:53 +0300

    created Categories component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import logoSvg from "./assets/img/pizza-logo.svg"
 
 import Header from './components/Header'
 import Button from './components/Button'
+import Categories from './components/Categories'
 
 function App() {
   return (
@@ -12,16 +13,7 @@ function App() {
       <div className="content">
         <div className="container">
           <div className="content__top">
-            <div className="categories">
-              <ul>
-                <li className="active">Все</li>
-                <li>Мясные</li>
-                <li>Вегетарианская</li>
-                <li>Гриль</li>
-                <li>Острые</li>
-                <li>Закрытые</li>
-              </ul>
-            </div>
+            <Categories />
             <div className="sort">
               <div className="sort__label">
                 <svg
