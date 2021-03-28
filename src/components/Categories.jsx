50188a76c686935142a09acef5b282577fcb0449
Ivan <ivanchernetskey@mail.ru>
2021-03-28 11:27:49 +0300

    created same component but like class, no like function

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,14 +1,34 @@
 import React from 'react'
 
-function Categories({ items, onClickItem}){
-    return(
-        <div className="categories">
-            <ul>
-                <li className="active">Все</li>
-                {items.map((item, index) => <li onClick={() => onClickItem(item)} key={`${item}_${index}`}>{item}</li>)}
-            </ul>
-        </div>
-    )
+//Классовый компонент
+
+class Categories extends React.Component {
+    render(){
+        const {items, onClickItem} = this.props
+        return(
+            <div className="categories">
+                <ul>
+                    <li className="active">Все</li>
+                    {items.map((item, index) => <li onClick={() => onClickItem(item)} key={`${item}_${index}`}>{item}</li>)}
+                </ul>
+            </div>
+        )
+    }
 }
+
+// function Categories({ items, onClickItem}){
+//
+//     //Работаем с хуками
+//
+//
+//     return(
+//         <div className="categories">
+//             <ul>
+//                 <li className="active">Все</li>
+//                 {items.map((item, index) => <li onClick={() => onClickItem(item)} key={`${item}_${index}`}>{item}</li>)}
+//             </ul>
+//         </div>
+//     )
+// }
 
 export default Categories
