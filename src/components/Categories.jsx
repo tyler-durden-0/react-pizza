520651acda15a5a261a92255f40ac3e2de3739c7
Ivan <ivanchernetskey@mail.ru>
2021-03-28 13:42:04 +0300

    use useState hook for make the same functional

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,51 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 //Классовый компонент
 
-class Categories extends React.Component {
-
-    state={
-        activeItem: 3
-    }
-
-    onSelectItem = index => {
-        this.setState({
-            activeItem: index
-        })
-    }
-
-    render(){
-        const {items, onClickItem} = this.props
-        return(
-            <div className="categories">
-                <ul>
-                    <li>Все</li>
-                    {items.map((item, index) => <li
-                        className={this.state.activeItem === index ? 'active' : ''}
-                        onClick={() => this.onSelectItem(index)}
-                        key={`${item}_${index}`
-                        }>
-                        {item}
-                    </li>)}
-                </ul>
-            </div>
-        )
-    }
-}
-
-// function Categories({ items, onClickItem}){
+// class Categories extends React.Component {
 //
-//     //Работаем с хуками
+//     state={
+//         activeItem: 3
+//     }
 //
+//     onSelectItem = index => {
+//         this.setState({
+//             activeItem: index
+//         })
+//     }
 //
-//     return(
-//         <div className="categories">
-//             <ul>
-//                 <li className="active">Все</li>
-//                 {items.map((item, index) => <li onClick={() => onClickItem(item)} key={`${item}_${index}`}>{item}</li>)}
-//             </ul>
-//         </div>
-//     )
+//     render(){
+//         const {items} = this.props
+//         return(
+//             <div className="categories">
+//                 <ul>
+//                     <li>Все</li>
+//                     {items.map((item, index) => <li
+//                         className={this.state.activeItem === index ? 'active' : ''}
+//                         onClick={() => this.onSelectItem(index)}
+//                         key={`${item}_${index}`
+//                         }>
+//                         {item}
+//                     </li>)}
+//                 </ul>
+//             </div>
+//         )
+//     }
 // }
+
+function Categories({ items, onClickItem}){
+
+    //Работаем с хуками
+    const [activeItem, setActiveItem] = useState(0)
+
+    return(
+        <div className="categories">
+            <ul>
+                <li>Все</li>
+                {items.map((item, index) => <li
+                    className={activeItem === index ? 'active' : ''}
+                    onClick={() => setActiveItem(index)}
+                    key={`${item}_${index}`}
+                >
+                    {item}
+                </li>)}
+            </ul>
+        </div>
+    )
+}
 
 export default Categories
