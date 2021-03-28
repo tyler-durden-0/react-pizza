e74076144cb908cde15bad8fd513a89c525b633c
Ivan <ivanchernetskey@mail.ru>
2021-03-28 11:18:34 +0300

    created logic in onClickItem for use him in onClick (look how we work with data)

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         <div className="container">
           <div className="content__top">
             <Categories
-                onClick={() => console.log('hello')}
+                onClickItem={name => console.log(name)}
                 items={[
                 'Мясные',
                 'Вегетарианская',
