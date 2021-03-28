6621c0122632bee9ce8d58a349dff0fe28a6c77b
Ivan <ivanchernetskey@mail.ru>
2021-03-28 11:39:25 +0300

    created method for highlight category in Categories

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -3,13 +3,30 @@ import React from 'react'
 //Классовый компонент
 
 class Categories extends React.Component {
+
+    state={
+        activeItem: 3
+    }
+
+    onSelectItem = index => {
+        this.setState({
+            activeItem: index
+        })
+    }
+
     render(){
         const {items, onClickItem} = this.props
         return(
             <div className="categories">
                 <ul>
-                    <li className="active">Все</li>
-                    {items.map((item, index) => <li onClick={() => onClickItem(item)} key={`${item}_${index}`}>{item}</li>)}
+                    <li>Все</li>
+                    {items.map((item, index) => <li
+                        className={this.state.activeItem === index ? 'active' : ''}
+                        onClick={() => this.onSelectItem(index)}
+                        key={`${item}_${index}`
+                        }>
+                        {item}
+                    </li>)}
                 </ul>
             </div>
         )
