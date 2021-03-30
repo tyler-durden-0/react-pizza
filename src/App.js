01f394732735d7d610cf0eaa75adff8c4fb79158
Ivan <ivanchernetskey@mail.ru>
2021-03-30 20:48:31 +0300

    created Cart component and import

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import React from 'react'
 import logoSvg from "./assets/img/pizza-logo.svg"
 
 import { Header } from './components'
-import {Home} from './pages'
+import { Home, Cart } from './pages'
 
 function App() {
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
-        <Home />
+        <Cart/>
       </div>
     </div>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
 import Home from './Home'
+import Cart from './Cart'
 
-export {Home}
+export { Home, Cart }
