bdeb6cd93d701ac9cd2601d7067d6f6dc26b3f33
Ivan <ivanchernetskey@mail.ru>
2021-03-30 21:19:03 +0300

    we can go back just for pizza_logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,13 +9,15 @@ function Header() {
     return (
         <div className="header">
             <div className="container">
-                <div className="header__logo">
-                    <img width="38" src={logoSvg} alt="Pizza logo" />
-                    <div>
-                        <h1>React Pizza</h1>
-                        <p>самая вкусная пицца во вселенной</p>
+                <Link to='/'>
+                    <div className="header__logo">
+                        <img width="38" src={logoSvg} alt="Pizza logo" />
+                        <div>
+                            <h1>React Pizza</h1>
+                            <p>самая вкусная пицца во вселенной</p>
+                        </div>
                     </div>
-                </div>
+                </Link>
                 <div className="header__cart">
                     <Link to='/cart'>
                         <Button className='button--cart'>
