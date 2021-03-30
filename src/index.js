7afc64de65d183a5be7591733b91e6fdf7f7a2b0
Ivan <ivanchernetskey@mail.ru>
2021-03-30 20:23:02 +0300

    try AS and Route

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 
 import './scss/app.scss';
 
 import App from './App';
+import Header from './components/Header'
 
 ReactDOM.render(
   <React.StrictMode>
       {/*если мы хотим использовать react-router - мы должны полностью обернуть наше приложение*/}
-      <BrowserRouter>
+      <Router>
           <Route exact path='/' component={App} />
-      </BrowserRouter>
+          <Route exact path='/header' component={Header} />
+      </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
