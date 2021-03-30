23fbb5e5aa9cf509b72aaf1f567da5976a857dce
Ivan <ivanchernetskey@mail.ru>
2021-03-30 20:40:40 +0300

    created Home component

--- src/components/index.js
+++ src/components/index.js
@@ -2,8 +2,9 @@ import Button from './Button'
 import Header from './Header'
 import Categories from './Categories'
 import SortPopup from './SortPopup'
+import Home from '../pages/Home'
 
-export { Button, Header, Categories, SortPopup }
+export { Button, Header, Categories, SortPopup, Home }
 
 //То же самое что и
 // export {
--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import {BrowserRouter as Router} from 'react-router-dom'
 
 import './scss/app.scss';
 
 import App from './App';
-import Header from './components/Header'
 
 ReactDOM.render(
   <React.StrictMode>
       {/*если мы хотим использовать react-router - мы должны полностью обернуть наше приложение*/}
+      {/*Route нельзя использовать вне BrowserRouter*/}
       <Router>
-          <Route exact path='/' component={App} />
-          <Route exact path='/header' component={Header} />
+          <App />
       </Router>
   </React.StrictMode>,
   document.getElementById('root')
