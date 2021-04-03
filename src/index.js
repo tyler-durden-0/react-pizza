f23fa0d53433ce1e24fdb4cb8ef7bd23d9c8e2dc
Ivan <ivanchernetskey@mail.ru>
2021-04-03 15:08:24 +0300

    example of Redux with button

--- src/index.js
+++ src/index.js
@@ -9,8 +9,17 @@ import App from './App';
 
 console.log(store)
 
+const inc = () => {
+    store.dispatch({type: 'INCREMENT'})
+}
+//когда какое-то изменение в сторе произойдет - оповети нас об этом
+store.subscribe(() => console.log('ИЗМЕНИЛСЯ! ', store.getState()))
+
+
+
 ReactDOM.render(
   <React.StrictMode>
+      <button onClick={inc}>+1</button>
       {/*если мы хотим использовать react-router - мы должны полностью обернуть наше приложение*/}
       {/*Route нельзя использовать вне BrowserRouter*/}
       <Router>
