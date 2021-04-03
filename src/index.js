137f158907426f7b5b0a50cbef3f23864b42117c
Ivan <ivanchernetskey@mail.ru>
2021-04-03 14:37:07 +0300

    example using Redux in React

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from 'react-router-dom'
+import { createStore } from 'redux'
 
 import './scss/app.scss';
 
 import App from './App';
+
+function counter(state = 0, action) {
+    switch(action.type) {
+        case 'INCREMENT':
+            return state + 1
+        case 'DECREMENT':
+            return state - 1
+        default:
+            return state
+    }
+}
+
+const store = createStore(counter)
+store.subscribe(() => console.log('хранилище изменилось ', store.getState()))
+console.log('текущее хначение ', store.getState())
+store.dispatch({type: "INCREMENT"})
+store.dispatch({type: "INCREMENT"})
+store.dispatch({type: "DECREMENT"})
+
 
 ReactDOM.render(
   <React.StrictMode>
