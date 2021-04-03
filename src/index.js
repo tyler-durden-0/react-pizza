e475ca90eaeddcea07f7e998ed0b824b3765d288
Ivan <ivanchernetskey@mail.ru>
2021-04-03 16:47:38 +0300

    use Provider from react-redux for link our store

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from 'react-router-dom'
 import store from './redux/store'
+import {Provider} from 'react-redux'
 
 import './scss/app.scss';
 
@@ -12,7 +13,9 @@ ReactDOM.render(
       {/*если мы хотим использовать react-router - мы должны полностью обернуть наше приложение*/}
       {/*Route нельзя использовать вне BrowserRouter*/}
       <Router>
-          <App />
+          <Provider store={store}>
+              <App />
+          </Provider>
       </Router>
   </React.StrictMode>,
   document.getElementById('root')
