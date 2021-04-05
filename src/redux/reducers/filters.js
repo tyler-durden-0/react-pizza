0c4b1afee1e0d75746c2cb63aec9b6a17588b2de
Ivan <ivanchernetskey@mail.ru>
2021-04-05 20:11:38 +0300

    added SET_CATEGORY case in filters reducer

--- src/redux/reducers/filters.js
+++ src/redux/reducers/filters.js
@@ -11,6 +11,12 @@ const filters = (state = initialState, action) => {
                 ...state,
                 sortBy: action.payload
             }
+        case 'SET_CATEGORY':
+            return {
+                //берем старые значения объекта и меняем в нем sortBy
+                ...state,
+                category: action.payload
+            }
         default:
             return state
     }
