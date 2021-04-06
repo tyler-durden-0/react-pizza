75ea04c00385ce457da3d9624c25e85afa1d49f4
Ivan <ivanchernetskey@mail.ru>
2021-04-06 11:39:54 +0300

    dispatch our action to delivery our pizzas in store

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 import {Route} from 'react-router-dom'
 
-//import logoSvg from "./assets/img/pizza-logo.svg"
+import store from './redux/store'
 
 import { Header } from './components'
 import { Home, Cart } from './pages'
+import { setPizzas } from './redux/actions/pizzas'
 
 // function App() {
 //     const [pizzas, setPizzas] = useState([])
@@ -30,6 +31,11 @@ import { Home, Cart } from './pages'
 // }
 
 class App extends React.Component {
+    //когда компонент будет первый раз отрендарин
+    componentDidMount() {
+        axios.get('http://localhost:3000/db.json').then(({data}) => store.dispatch(setPizzas(data.pizzas)))
+    }
+
     render() {
         return(
             <div className="wrapper">
