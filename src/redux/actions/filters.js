704840d3aa3d182b3b93a78f7625aa1d364a81f0
Ivan <ivanchernetskey@mail.ru>
2021-04-07 12:56:42 +0300

    action & action creator

--- src/redux/actions/filters.js
+++ src/redux/actions/filters.js
@@ -1,9 +1,17 @@
+//Action creator
 const setSortBy = (name) => {
     return {
         type: 'SET_SORT_BY',
         payload: name
     }
 }
+
+//Action
+// const setSortBy = {
+//     return {
+//         type: 'SET_SORT_BY'
+//     }
+// }
 
 //немного другим способом пишим action, чем setSortBy
 const setCategory = (catIndex) => ({
