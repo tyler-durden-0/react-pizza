c43e20112c71e4e5a80d22d51357868389dd8760
Ivan <ivanchernetskey@mail.ru>
2021-04-17 14:54:16 +0300

    wow, we created our own react-content-loader, and it looks so cool&awesome

--- src/pages/PizzaBlock.jsx
+++ src/pages/PizzaBlock.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import classNames from 'classnames'
 import PropTypes from 'prop-types'
+import ContentLoader from 'react-content-loader'
 
 function PizzaBlock({ name, imageUrl, price, types, sizes }) {
     const availableTypes = ['тонкое', 'традиционное']
@@ -15,6 +16,23 @@ function PizzaBlock({ name, imageUrl, price, types, sizes }) {
     const onSelectSize = (size) => {
         setActiveSize(size)
     }
+
+    return (
+        <ContentLoader
+            speed={2}
+            width={280}
+            height={460}
+            viewBox="0 0 280 460"
+            backgroundColor="#f3f3f3"
+            foregroundColor="#ecebeb"
+        >
+            <circle cx="132" cy="142" r="115" />
+            <rect x="0" y="273" rx="6" ry="6" width="280" height="26" />
+            <rect x="0" y="310" rx="6" ry="6" width="280" height="84" />
+            <rect x="0" y="418" rx="6" ry="6" width="91" height="31" />
+            <rect x="137" y="408" rx="25" ry="25" width="140" height="46" />
+        </ContentLoader>
+    )
 
     return(
         <div className="pizza-block">
