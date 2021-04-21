047e60f8ecf4b0109fc1b5e38a5c770330bcc124
Ivan <ivanchernetskey@mail.ru>
2021-04-21 15:55:02 +0300

    make action addPizzaToCart, modify reducer for Cart component

--- src/redux/reducers/cart.js
+++ src/redux/reducers/cart.js
@@ -6,17 +6,16 @@ const initialState = {
 
 const cart = (state = initialState, action) => {
     switch(action.type) {
-        case 'SET_TOTAL_PRICE':
+        case 'ADD_PIZZA_TYPE':
             return {
                 //берем старые значения объекта и меняем в нем totalPrice
                 ...state,
-                totalPrice: action.payload
-            }
-        case 'SET_TOTAL_COUNT':
-            return {
-                //берем старые значения объекта и меняем в нем totalCount
-                ...state,
-                totalCount: action.payload
+                items: {
+                    [action.payload.id]: [
+                        ...state.items[action.payload.id],
+                        action.payload
+                    ]
+                }
             }
         default:
             return state
