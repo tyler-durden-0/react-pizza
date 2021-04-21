8f222bc206805001881f6b0ebea4d4281a78f565
Ivan <ivanchernetskey@mail.ru>
2021-04-21 12:21:41 +0300

    added cartReducer into rootReducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,10 +2,16 @@ import { combineReducers } from 'redux'
 
 import filtersReducer from './filters'
 import pizzasReducer from './pizzas'
+import cartReducer from './cart'
 
 const rootReducer = combineReducers({
     filters: filtersReducer,
-    pizzas: pizzasReducer
+    pizzas: pizzasReducer,
+    cart: cartReducer
+    //можно записать благодаря ES-6 как
+    //filters,
+    //pizzas,
+    //cart
 })
 
 export default rootReducer
