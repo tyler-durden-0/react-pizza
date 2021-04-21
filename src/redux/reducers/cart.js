ba8de35b9f083cb1fb3f6082e4f3f41543e8132a
Ivan <ivanchernetskey@mail.ru>
2021-04-21 12:01:51 +0300

    make cart reducer in Redux

---
+++ src/redux/reducers/cart.js
@@ -0,0 +1,26 @@
+const initialState = {
+    items: [],
+    totalPrice: 0,
+    itemsCount: 0
+}
+
+const cart = (state = initialState, action) => {
+    switch(action.type) {
+        case 'SET_TOTAL_PRICE':
+            return {
+                //берем старые значения объекта и меняем в нем totalPrice
+                ...state,
+                totalPrice: action.payload
+            }
+        case 'SET_TOTAL_COUNT':
+            return {
+                //берем старые значения объекта и меняем в нем totalCount
+                ...state,
+                totalCount: action.payload
+            }
+        default:
+            return state
+    }
+}
+
+export default cart
