f016d1e3f4dec4b8a3014ffdb2459a263727d1ff
Ivan <ivanchernetskey@mail.ru>
2021-04-23 19:37:35 +0300

    modify ADD_PIZZA_TYPE reducer for correct totalCount

--- src/redux/reducers/cart.js
+++ src/redux/reducers/cart.js
@@ -7,15 +7,21 @@ const initialState = {
 const cart = (state = initialState, action) => {
     switch(action.type) {
         case 'ADD_PIZZA_TYPE':
+            //здесь я храню актуальные данные при вызове этого экшэна
+            const newItems = {
+                ...state.items,
+                [action.payload.id]: !state.items[action.payload.id]
+                    ? [action.payload]
+                    : [...state.items[action.payload.id], action.payload]
+            }
+
             return {
                 //берем старые значения объекта и меняем в нем totalPrice
                 ...state,
-                items: {
-                    ...state.items,
-                    [action.payload.id]: !state.items[action.payload.id]
-                        ? [action.payload]
-                        : [...state.items[action.payload.id], action.payload]
-                }
+                items: newItems,
+                //очень хитрая логика досчета количества всех массивов, для пределения количества добавленных пицц
+                //прочитать про concat и apply
+                totalCount: [].concat.apply([], Object.values(newItems)).length
             }
         default:
             return state
