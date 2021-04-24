0d3b108307bbef2350b389f91429a1c5a27999cc
Ivan <ivanchernetskey@mail.ru>
2021-04-24 11:31:15 +0300

    added proxy in package.json in order to redirect request

--- src/redux/actions/pizzas.js
+++ src/redux/actions/pizzas.js
@@ -9,7 +9,7 @@ export const setLoaded = (val) => ({
 export const fetchPizzas = (category, sortBy) => (dispatch) => {
     dispatch(setLoaded(false))
     axios.get(
-        `http://localhost:3001/pizzas?${category !== null ? `category=${category}` : ''}&_sort=${sortBy.type}&_order=${sortBy.order}`).then(({data}) => {
+        `/pizzas?${category !== null ? `category=${category}` : ''}&_sort=${sortBy.type}&_order=${sortBy.order}`).then(({data}) => {
         dispatch(setPizzas(data))
     })
 }
